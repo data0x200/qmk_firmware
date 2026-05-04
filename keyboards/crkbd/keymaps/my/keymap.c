a5f66dd88fbacec295cf7e393440c677419700b1
Daichi Takamiya <data0x200@gmail.com>
2026-05-04 19:11:28 +0900

    update
    mac os switch

--- keyboards/crkbd/keymaps/my/keymap.c
+++ keyboards/crkbd/keymaps/my/keymap.c
@@ -18,8 +18,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+enum layers {
+    _LINUX_MAIN,
+    _LINUX_NUMBER,
+    _LINUX_CONTROL,
+    _LINUX_OTHER,
+    _MAC_MAIN,
+    _MAC_NUMBER,
+    _MAC_CONTROL,
+    _MAC_OTHER,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_split_3x6_3(
+  [_LINUX_MAIN] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_GRV,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_LBRC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -27,12 +38,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RBRC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            MO(2), KC_LGUI,  KC_SPC,     KC_ENT, KC_RALT,   MO(1)
+                              MO(_LINUX_CONTROL), KC_LGUI,  KC_SPC,    KC_ENT, KC_RALT,   MO(_LINUX_NUMBER)
                                       //`--------------------------'  `--------------------------'
 
   ),
 
-  [1] = LAYOUT_split_3x6_3(
+  [_LINUX_NUMBER] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, G(KC_TAB),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -40,34 +51,122 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,  KC_F11,  KC_F12, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            MO(3), KC_LGUI,  KC_SPC,     KC_ENT, KC_RALT, _______
+                                MO(_LINUX_OTHER), KC_LGUI,  KC_SPC,     KC_ENT, KC_RALT, _______
                                       //`--------------------------'  `--------------------------'
   ),
 
-  [2] = LAYOUT_split_3x6_3(
+  [_LINUX_CONTROL] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC, XXXXXXX, KC_PGUP, XXXXXXX,   KC_UP, XXXXXXX,                      KC_RGHT, KC_PSCR, XXXXXXX, KC_MINS,  KC_EQL, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, KC_HOME, KC_PGDN,  KC_END,  KC_TAB,  KC_TAB,                       KC_DEL, KC_BSPC,  KC_INS, KC_DOWN, S(KC_Z), XXXXXXX,
+      KC_LCTL, KC_HOME, KC_PGDN,  KC_END,  KC_TAB,  KC_TAB,                       KC_DEL, KC_BSPC,  KC_INS, KC_DOWN, C(KC_S), XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT,  KC_ENT, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS,
+      KC_LSFT, KC_UNDO,  KC_CUT, KC_COPY,KC_PASTE, XXXXXXX,                      KC_LEFT,  KC_ENT, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, KC_LGUI,  KC_SPC,     KC_ENT, KC_RALT,   MO(3)
+                                         _______, KC_LGUI,  KC_SPC,    KC_ENT, KC_RALT,   MO(_LINUX_OTHER)
                                       //`--------------------------'  `--------------------------'
   ),
 
-  [3] = LAYOUT_split_3x6_3(
+  [_LINUX_OTHER] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC, QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LAUNCHPAD, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_MISSION_CONTROL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_MISSION_CONTROL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, KC_LGUI,  KC_SPC,     KC_ENT, KC_RALT, _______
+                                         _______, KC_LGUI,  KC_SPC,    KC_ENT, KC_RALT, _______
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+  [_MAC_MAIN] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_GRV,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_LBRC,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RBRC,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                MO(_MAC_CONTROL), KC_LALT,  KC_SPC,    KC_ENT, KC_LGUI, MO(_MAC_NUMBER)
+                                      //`--------------------------'  `--------------------------'
+
+  ),
+
+  [_MAC_NUMBER] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, G(KC_TAB),
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LCTL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT,  KC_F11,  KC_F12, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                  MO(_MAC_OTHER), KC_LALT,  KC_SPC,    KC_ENT, KC_LGUI, _______
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+  [_MAC_CONTROL] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_ESC, XXXXXXX, KC_PGUP, XXXXXXX,   KC_UP, XXXXXXX,                      KC_RGHT, KC_PSCR, XXXXXXX, KC_MINS,  KC_EQL, KC_BSLS,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LCTL, KC_HOME, KC_PGDN,  KC_END,  KC_TAB,  KC_TAB,                       KC_DEL, KC_BSPC,  KC_INS, KC_DOWN, C(KC_S), XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT, KC_UNDO,  KC_CUT, KC_COPY,KC_PASTE, XXXXXXX,                      KC_LEFT,  KC_ENT, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                         _______, KC_LALT,  KC_SPC,    KC_ENT, KC_LGUI, MO(_MAC_NUMBER)
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+  [_MAC_OTHER] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_ESC, QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LAUNCHPAD, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_MISSION_CONTROL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_LGUI, KC_LALT,  KC_SPC,    KC_ENT, _______, _______
                                       //`--------------------------'  `--------------------------'
   )
 };
+
+#if defined(OS_DETECTION_ENABLE) && defined(DEFERRED_EXEC_ENABLE)
+#include "os_detection.h"
+os_variant_t os_type;
+
+uint32_t detect_os(uint32_t trigger_time, void *cb_arg) {
+    if (is_keyboard_master()) {
+        os_type = detected_host_os();
+        if (os_type) {
+            switch (os_type) {
+                case OS_MACOS:
+                    layer_move(_MAC_MAIN);
+                    break;
+                case OS_IOS:
+                    layer_move(_MAC_MAIN);
+                    break;
+                case OS_LINUX:
+                    layer_move(_LINUX_MAIN);
+                    break;
+                case OS_WINDOWS:
+                    layer_move(_LINUX_MAIN);
+                    break;
+                case OS_UNSURE:
+                    layer_move(_LINUX_MAIN);
+                    break;
+                default:
+                    layer_move(_LINUX_MAIN);
+                    break;
+            }
+        }
+    }
+
+    return os_type ? 0 : 500;
+}
+
+void keyboard_post_init_user(void) {
+    defer_exec(100, detect_os, NULL);
+}
+#endif
 
 #ifdef OLED_DRIVER_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
